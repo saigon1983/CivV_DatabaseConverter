16c7be61419f0217470c7738f3e12ce5b958274f
saigon1983 <rodionov.homeappliance@gmail.com>
2023-11-10 12:40:08 +0300

    Добавлены функции SQL-обработки get_row_names() get_table_names()

--- SQL_Module/sql_main.py
+++ SQL_Module/sql_main.py
@@ -1,22 +1,21 @@
 import sqlite3
 
-connection = sqlite3.connect('Original/Database/Civ5DebugDatabase.db')
-connection.row_factory = sqlite3.Row
-
-cursor = connection.cursor()
-
-cursor = connection.execute('select * from buildings')
-cursor = connection.execute("SELECT name, type FROM pragma_table_info('buildings')")
-results = cursor.description
-row = cursor.fetchone()
-names = row.keys()
-
-print(names)
-
-
-
-connection.close()
-
 def get_row_names(tablename, database = 'Original/Database/Civ5DebugDatabase.db'):
+  connection = sqlite3.connect(database)
+  connection.row_factory = sqlite3.Row
+  cursor = connection.cursor()
+  cursor = connection.execute('select * from {}'.format(tablename))
+  row = cursor.fetchone()
+  names = row.keys()
+  connection.close()
+  return names
 
-  pass
+def get_table_names(database = 'Original/Database/Civ5DebugDatabase.db'):
+  connection = sqlite3.connect(database)
+  cursor = connection.cursor()
+  cursor.execute('SELECT name from sqlite_master where type= "table"')
+  return [item[0] for item in cursor.fetchall()]
+
+rows = get_row_names('Units')
+tables = get_table_names()
+print(tables)
