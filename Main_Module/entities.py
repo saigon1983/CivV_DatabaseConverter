528fcf6a598affdf5cd679c5cd669f1e26137406
Saigon1983 <rodionov.homeappliance@gmail.com>
2023-11-13 18:27:45 +0300

    Добавлены функции SQL-обработки get_column_properties

--- Main_Module/entities.py
+++ Main_Module/entities.py
@@ -1,5 +1,6 @@
 from SQL_Module.sql_main import *
 from Main_Module.constants import *
+import re
 
 class CivValue:
 
@@ -64,8 +65,19 @@ class Civ5Entity:
 	DB 	= sqlite3.connect('Original/Database/Civ5DebugDatabase.db')
 	MainTableName 		= ''	# Название основной таблицы (тип сущности)
 	DatabaseTableNames 	= [item[0] for item in DB.execute('SELECT name from sqlite_master where type= "table"').fetchall()]
-	SecondaryTablePrefixes = {'Units': 'Unit',
-							  'Buildings': 'Building'}
+	SecondaryTablePrefixes = {'BuildingClasses': 'BuildingClass',
+							  'Buildings': 			'Building',
+							  'Civilizations': 		'Civilization',
+							  'Eras': 				'Era',
+							  'Features': 			'Feature',
+							  'Improvements': 		'Improvement',
+							  'Leaders': 			'Leader',
+							  'Policies': 			'Policy',
+							  'PolicyBranchTypes': 	'PolicyBranch',
+							  'Resources': 			'Resource',
+							  'Technologies': 		'Technology',
+							  'UnitClasses': 		'UnitClass',
+							  'Units': 				'Unit'}
 	ValidTables 		= {}	# Словарь названий допустимых таблиц, в которых может быть отредактирована сущность.
 								# Ключами являются названия таблиц, а значениями - вложенные списки, где ключами являются
 								# заголовки свойств (столбцов), а их значениями - параметры этих заголовков в БД SQL
@@ -73,7 +85,6 @@ class Civ5Entity:
 	@classmethod
 	def set_main_table_name(cls, value):
 		cls.MainTableName = value
-
 	@classmethod
 	def set_valid_tables(cls, value):
 		tablenames = value.copy()
@@ -83,6 +94,8 @@ class Civ5Entity:
 				tablename = '{}_{}'.format(prefix, tablename)
 
 				if tablename == 'Building_DomainFreeExperiencePerGW': tablename = 'Building_DomainFreeExperiencePerGreatWork'
+				if tablename == 'Feature_FakeFeatures': tablename = 'FakeFeatures'
+				if tablename == 'Policy_BuildinClassProductionModifiers': tablename = 'Policy_BuildingClassProductionModifiers'
 
 			if tablename not in cls.DatabaseTableNames:
 				raise ValueError('Таблицы с именем <{}> нет в оригинальной базе данных!'.format(tablename))
@@ -93,30 +106,180 @@ class Civ5Entity:
 			i2 = sqlfile[i1:].find(');')
 			sqlfile = sqlfile[i1:i1+i2].splitlines()
 
-			columnName 		= None
-			dataType 		= None
-			defaultValue 	= None
-
-
 			for line in sqlfile[1:]:
-				line = line.strip().rstrip(',')
-				subline = line.split('\t')
-				if line.startswith('"'):
-					columnName = subline[0].strip('"')
-					subline = subline[1:]
-					if subline[0].strip("'").startswith('text'):
-						print(columnName, subline)
+				columnName 		= None
+				dataType 		= None
+				defaultValue	= None
+				notNull			= False
+				unique			= False
 
-				elif line.startswith('FOREIGN KEY'):
-					pass
-				else:
-					pass
+				line = line.strip().rstrip(',')
+				match = re.match(r'"\S+"\t', line)
+				if match:
+					line = line.replace(match[0], '').strip()
+					columnName = match[0][1:-2]
+				if line.startswith('FOREIGN KEY'):
+					match = re.match(r'FOREIGN KEY\("\S+"\)', line)[0]
+					line = line.replace(match, '').strip()
+					match = match.replace('FOREIGN KEY("', '').replace('")', '').strip()
+					columnName = match
+				if line.startswith('PRIMARY KEY'):
+					line = line.replace('PRIMARY KEY', '').strip('(').strip(')').strip()
+					match = re.match(r'"\S+"', line)[0]
+					line = line.replace(match, '').strip()
+					columnName = match.replace('"', '')
+				if not columnName: raise ValueError
+				if columnName not in cls.ValidTables[tablename].keys():
+					cls.ValidTables[tablename][columnName] = {'DataType': 		None,
+															  'DefaultValue': 	None,
+															  'References': 	None,
+															  'AutoIncrement': 	None,
+															  'NotNull': 		None,
+															  'Unique': 		None}
+				if line.startswith('AUTOINCREMENT'):
+					line = line.replace('AUTOINCREMENT', '').strip()
+					cls.ValidTables[tablename][columnName]['AutoIncrement'] = True
+				if line.startswith('REFERENCES'):
+					match = re.match(r'REFERENCES "\S+"\("\S+"\)', line)[0]
+					line = line.replace(match, '').strip()
+					match = match.replace('REFERENCES "', '').replace('")', ')').replace('"("', '(').strip()
+					cls.ValidTables[tablename][columnName]['References'] = match
+				if line.startswith('text'):
+					dataType = 'TEXT'
+					line = line.replace('text', '').strip()
+				if line.startswith('integer') or line.startswith('int'):
+					dataType = 'INTEGER'
+					line = line.replace('integer', '').replace('int', '').strip()
+				if line.startswith('boolean') or line.startswith('bool'):
+					dataType = 'BOOLEAN'
+					line = line.replace('boolean', '').replace('bool', '').strip()
+				if not cls.ValidTables[tablename][columnName]['DataType']:
+					cls.ValidTables[tablename][columnName]['DataType'] = dataType
+				if line.startswith('DEFAULT'):
+					line = line.replace('DEFAULT', '').strip()
+					if line == '0' and dataType == 'BOOLEAN':
+						defaultValue = 'false'
+						line = line.replace('0', '').strip()
+					if line == '1' and dataType == 'BOOLEAN':
+						defaultValue = 'true'
+						line = line.replace('1', '').strip()
+					if line == 'NULL' and dataType == 'TEXT':
+						defaultValue = 'NULL'
+						line = line.replace(defaultValue, '').strip()
+					if line == "'-1'" and dataType == 'INTEGER':
+						defaultValue = -1
+						line = line.replace("'-1'", '').strip()
+					try:
+						defaultValue = int(line)
+						line = line.replace(str(defaultValue), '').strip()
+					except: pass
+					if line.startswith("'"):
+						defaultValue = line.strip("'")
+						line = line.replace(defaultValue, '').replace("'", '').strip()
+				if not cls.ValidTables[tablename][columnName]['DefaultValue']:
+					cls.ValidTables[tablename][columnName]['DefaultValue'] = defaultValue
+				if line.startswith('NOT NULL'):
+					line = line.replace("NOT NULL", '').strip()
+					notNull = True
+				if line.startswith('UNIQUE'):
+					line = line.replace("UNIQUE", '').strip()
+					unique = True
+				cls.ValidTables[tablename][columnName]['NotNull'] 		= notNull
+				cls.ValidTables[tablename][columnName]['Unique'] 		= unique
+				if line:
+					raise ValueError
 
 	def __init__(self, someData):
 		for key, value in someData.items():
 			if key == 'EntityType':
 				if not self.__class__.MainTableName: self.set_main_table_name(value)
-			if key == 'TableNames':
-				if not self.__class__.ValidTables:
-					self.set_valid_tables(value)
-					print(self.ValidTables)
+			elif key == 'TableNames':
+				if not self.__class__.ValidTables: self.set_valid_tables(value)
+			else:
+				curColumn 	= key
+				curValue  	= value
+				curAttribs	= self.ValidTables[self.MainTableName][key]
+
+				if curAttribs['DataType'] == 'INTEGER':
+					try: 	curValue = int(curValue)
+					except: pass
+					if curValue and type(curValue) != int: raise ValueError
+				elif curAttribs['DataType'] == 'TEXT': pass
+				elif curAttribs['DataType'] == 'BOOLEAN':
+					if curValue not in ('false', 'true') and curValue != None:
+						raise TypeError('Table <{}>: column <{}> has value <{}>. Must be in <<false, true>>'.format(self.MainTableName, curColumn, curValue))
+				else: raise TypeError('Table <{}>: Unrecognized data type <{}> in column <{}>'.format(self.MainTableName, curAttribs['DataType'], curColumn))
+
+				if curValue == None:
+					#if curValue != curAttribs['DefaultValue']: print(self.MainTableName, curColumn, curValue, curAttribs['DefaultValue'])
+					curValue = curAttribs['DefaultValue']
+				if curValue == None:
+					pass
+					print(self.MainTableName, curColumn, curValue)
+				print('====================================')
+
+class BuildingClass(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Building(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Civilization(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Era(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Feature(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Improvement(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Leader(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Policy(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class PolicyBranch(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Resource(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Technology(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class UnitClass(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
+class Unit(Civ5Entity):
+	MainTableName	= ''
+	ValidTables 	= {}
+	def __init__(self, someData):
+		super().__init__(someData)
--- main.py
+++ main.py
@@ -2,9 +2,39 @@ from SQL_Module.sql_main import *
 from XLS_Module.xls_main import *
 from Main_Module.entities import *
 
+tables = ('BuildingClasses', 'Buildings', 'Civilizations', 'Eras', 'Features', 'Improvements',
+		  'Leaders', 'Policies', 'PolicyBranchTypes', 'Resources', 'Technologies', 'UnitClasses', 'Units')
 
-for entity in select_all_entities('Buildings').values():
-	newEntity = Civ5Entity(entity)
+for table in tables:
+	print('---------- >>>>> Идет обработка таблицы {}'.format(table))
+	for entity in select_all_entities(table).values():
+		if table == 'BuildingClasses':
+			newEntity = BuildingClass(entity)
+		if table == 'Buildings':
+			newEntity = Building(entity)
+		if table == 'Civilizations':
+			newEntity = Civilization(entity)
+		if table == 'Eras':
+			newEntity = Era(entity)
+		if table == 'Features':
+			newEntity = Feature(entity)
+		if table == 'Improvements':
+			newEntity = Improvement(entity)
+		if table == 'Leaders':
+			newEntity = Leader(entity)
+		if table == 'Policies':
+			newEntity = Policy(entity)
+		if table == 'PolicyBranchTypes':
+			newEntity = PolicyBranch(entity)
+		if table == 'Resources':
+			newEntity = Resource(entity)
+		if table == 'Technologies':
+			newEntity = Technology(entity)
+		if table == 'UnitClasses':
+			newEntity = UnitClass(entity)
+		if table == 'Units':
+			newEntity = Unit(entity)
+
 
 
 Civ5Entity.DB.close()
