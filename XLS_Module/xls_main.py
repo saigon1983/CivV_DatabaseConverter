25da4c227a74f3b81d4722678d43ebbc57d110e5
Saigon1983 <rodionov.homeappliance@gmail.com>
2023-11-20 18:26:42 +0300

    1

--- XLS_Module/xls_main.py
+++ XLS_Module/xls_main.py
@@ -50,45 +50,6 @@ def get_main_table_headers(tablename, original = True):
 		pos += 1
 		labels[cell.value] = pos
 	return labels
-def select_all_entities(tablename, original = True):
-	WB = get_tables(tablename, original)
-	WSprimary 	= WB[WB.sheetnames[0]]
-	WSsecondary = WB.sheetnames[1:]
-	HEADERS  = {}
-	ENTITIES = {}
-	for cell in WSprimary[1]: HEADERS[cell.value] = cell.column
-
-	for row_index in range(3, WSprimary.max_row+1):
-		entity_name = WSprimary[row_index][HEADERS['Type']-1].value
-		ENTITIES[entity_name] = {'EntityType': tablename,
-								 'TableNames': WB.sheetnames}
-		for key, val in HEADERS.items():
-			address = '{}{}'.format(get_column_letter(val), row_index)
-			if key: ENTITIES[entity_name][key] = WSprimary[address].value
-
-	for key in ENTITIES.keys():
-		ENTITIES[key]['SecondaryTables'] = {}
-		for tableName in ENTITIES[key]['TableNames'][1:]:
-			ENTITIES[key]['SecondaryTables'][tableName] = []
-
-	for secondary in WSsecondary:
-		WS = WB[secondary]
-		WStype = WS['A2'].value
-		if WStype == 'OneType_OneStatement':
-			tagMain 	= WS['A1'].value
-			tagSecond 	= WS['B1'].value
-			for rowNum in range(3, WS.max_row+1):
-				someType  = WS['A{}'.format(rowNum)].value
-				someValue = WS['B{}'.format(rowNum)].value
-				if someValue:
-					ENTITIES[someType]['SecondaryTables'][secondary].append(someValue)
-					print(ENTITIES[someType])
-					#print(secondary, someType, someValue)
-		else:
-			pass
-
-		#if WStype: print(secondary, WStype)
-	return ENTITIES
 
 def get_proper_subtable_name(mainTableName, tablename):
 	result = '{}_{}'.format(SecondaryTablePrefixes[mainTableName], tablename)
@@ -114,14 +75,13 @@ def parse_excel_Civ5_table(tablename, original = True):
 	for row_index in range(3, mainTable.max_row + 1):
 		name 		= mainTable[row_index][headers['Type'] - 1].value
 		ENTITIES[name] = {'EntityType': mainTableName,
-						  'TableNames': properSubNames,
-						  'Subtables':	{}}
-		for subtablename in ENTITIES[name]['TableNames']:
-			ENTITIES[name]['Subtables'][subtablename] = {'TableType': 	None,
-														 'TableValues':	[]}
+						  mainTableName: {}}
+		for subname in properSubNames:
+			ENTITIES[name][subname] = {'TableType': 	None,
+									   'TableValues':	[]}
 		for key, val in headers.items():
 			address = '{}{}'.format(get_column_letter(val), row_index)
-			if key: ENTITIES[name][key] = mainTable[address].value
+			if key: ENTITIES[name][mainTableName][key] = mainTable[address].value
 	# Приступаем к обработке второстепенных таблиц
 	for subtablename in subTableNames:
 		subTable 	 	= workbook[subtablename]
@@ -129,20 +89,74 @@ def parse_excel_Civ5_table(tablename, original = True):
 		subTableType 	= subTable['A2'].value
 		subTableValues 	= {}
 
-		if subTableType == 'OneType_OneStatement':
+		if subTableType == 'OneType_HeadersState_Reference':
 			for rowNum in range(3, subTable.max_row + 1):
-				entityType	= subTable['A{}'.format(rowNum)].value
+				entityName	= subTable['A{}'.format(rowNum)].value
 				properValue	= subTable['B{}'.format(rowNum)].value
 				if properValue:
-					if subTableKey not in ENTITIES[entityType]['Subtables'].keys(): raise TypeError
-					ENTITIES[entityType]['Subtables'][subTableKey]['TableType'] = subTableType
-					ENTITIES[entityType]['Subtables'][subTableKey]['TableValues'].append(properValue)
-		elif subTableType == 'OneType_PredefineStatement':
-			valuesRange = range(2, subTable.max_column + 1)
+					if subTableKey not in ENTITIES[entityName].keys(): raise TypeError
+					ENTITIES[entityName][subTableKey]['TableType'] = subTableType
+					ENTITIES[entityName][subTableKey]['TableValues'].append(properValue)
+		elif subTableType == 'OneType_HeadersValue_Quantable':
 			thisHeaders = {}
-			for cell in subTable[1]: thisHeaders[cell.value] = cell.column
-			print(thisHeaders)
-			print(subTableKey, valuesRange)
+			for cell in subTable[1]:
+				if cell.column != 1: thisHeaders[cell.value] = cell.column
+			for row_index in range(3, subTable.max_row + 1):
+				entityName	= subTable['A{}'.format(row_index)].value
+				properValues = {}
+				for key, val in thisHeaders.items():
+					address = '{}{}'.format(get_column_letter(val), row_index)
+					cellValue = subTable[address].value
+					if cellValue: properValues[key] = cellValue
+				if properValues:
+					ENTITIES[entityName][subTableKey]['TableType'] = subTableType
+					ENTITIES[entityName][subTableKey]['TableValues'] = properValues.copy()
+		elif subTableType == 'OneType_HeadersValue_Check':
+			thisHeaders = {}
+			for cell in subTable[1]:
+				if cell.column != 1: thisHeaders[cell.value] = cell.column
+			for row_index in range(3, subTable.max_row + 1):
+				entityName	= subTable['A{}'.format(row_index)].value
+				properValues = []
+				for key, val in thisHeaders.items():
+					address = '{}{}'.format(get_column_letter(val), row_index)
+					cellValue = subTable[address].value
+					if cellValue: properValues.append(key)
+				if properValues:
+					ENTITIES[entityName][subTableKey]['TableType'] = subTableType
+					ENTITIES[entityName][subTableKey]['TableValues'] = properValues.copy()
+		elif subTableType == 'MultiType_HeadersValue_Quantable':
+			thisHeaders = {}
+			for cell in subTable[1]:
+				if cell.column > 2: thisHeaders[cell.value] = cell.column
+			for row_index in range(3, subTable.max_row + 1):
+				entityName	= subTable['A{}'.format(row_index)].value
+				secondCol	= subTable['B{}'.format(row_index)].value
+				properValues = []
+				if secondCol:
+					for key, val in thisHeaders.items():
+						address = '{}{}'.format(get_column_letter(val), row_index)
+						cellValue = subTable[address].value
+						if cellValue:
+							properValues.append((key, secondCol, cellValue))
+				if properValues:
+					ENTITIES[entityName][subTableKey]['TableType'] = subTableType
+					for x in properValues: ENTITIES[entityName][subTableKey]['TableValues'].append(x)
+		elif subTableType == 'MultiType_HeadersState_Quantable':
+			thisHeaders = {}
+			for cell in subTable[1]:
+				if cell.column > 1: thisHeaders[cell.value] = cell.column
+			for row_index in range(3, subTable.max_row + 1):
+				entityName	= subTable['A{}'.format(row_index)].value
+				secondCol	= subTable['B{}'.format(row_index)].value
+				properValues = []
+				if secondCol:
+					for key, val in thisHeaders.items():
+						address = '{}{}'.format(get_column_letter(val), row_index)
+						cellValue = subTable[address].value
+						if cellValue:
+							#properValues.append((key, secondCol, cellValue))
+							print(entityName, secondCol, cellValue)
 		else:
 			pass
 
